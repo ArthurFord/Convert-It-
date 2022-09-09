3974a299ac7e95efa6a1620d11e95e8017b1e216
Arthur Ford <adf.ford@gmail.com>
2022-09-09 16:02:31 -0400

    Initial Commit

---
+++ Convert It!/Convert_It_App.swift
@@ -0,0 +1,17 @@
+//
+//  Convert_It_App.swift
+//  Convert It!
+//
+//  Created by Arthur Ford on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Convert_It_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
